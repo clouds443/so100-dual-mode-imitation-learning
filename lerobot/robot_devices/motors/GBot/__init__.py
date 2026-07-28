6d1bbb4b491e77576b9df1ae94410edead5660d0
clouds443 <2074185454@qq.com>
2026-07-28 13:36:52 +0800

    Delete lerobot/robot_devices directory

--- lerobot/robot_devices/motors/GBot/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from .port_handler import PortHandler
-from .global_state import Result, Address
-from .sync_connector import SyncConnector
--- lerobot/robot_devices/robots/tt.py
+++
@@ -1,2 +0,0 @@
-import feetech_calibration
-feetech_calibration.run_arm_manual_calibration(arm, "so100", "leader_arms", "main_leader")
